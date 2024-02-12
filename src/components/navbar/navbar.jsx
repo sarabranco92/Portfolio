00d14f6054711b4fcb47e044f189f939aa5fd318
sarabranco92 <sara1992@live.com.pt>
2024-02-12 17:37:17 +0100

    add responsive layout

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,17 +1,15 @@
-
 import React, { useState, useEffect } from 'react';
-import Logo from "/assets/logo.png";
 import { Link } from 'react-router-dom';
-import './_Navbar.scss';
+import Logo from "/assets/logo.png";
 import cvFile from '../../../public/assets/Sara_BrancoCV.pdf';
+import './_Navbar.scss';
 
 const sections = ['#presentation', '#portfolio', '#skills', '#contact'];
 
 const Navbar = () => {
   const [activeSection, setActiveSection] = useState('');
   const [isAfterVanta, setIsAfterVanta] = useState(false);
-
-
+  const [isNavExpanded, setIsNavExpanded] = useState(false); // New state for mobile nav expansion
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -38,10 +36,8 @@ const Navbar = () => {
     };
   }, []);
 
-
   useEffect(() => {
     const handleScroll = () => {
-     
       const shouldBeFixed = window.scrollY > window.innerHeight;
       setIsAfterVanta(shouldBeFixed);
     };
@@ -53,11 +49,20 @@ const Navbar = () => {
     };
   }, []);
 
+  // Function to toggle the mobile navigation
+  const toggleNav = () => {
+    setIsNavExpanded(!isNavExpanded);
+  };
 
   return (
     <nav className={`navbar ${isAfterVanta ? 'is-after-vanta' : ''}`}>
-      <Link to="/" className="navbar__logo"> <img src={Logo} alt="Logo" /></Link>
-      <div className="navbar__links">
+      <Link to="/" className="navbar__logo">
+        <img src={Logo} alt="Logo" />
+      </Link>
+      <button className="navbar__toggle" onClick={toggleNav}>
+        <i className="fas fa-bars"></i> 
+      </button>
+      <div className={`navbar__links ${isNavExpanded ? 'is-expanded' : ''}`}>
         {sections.map((section) => {
           const sectionId = section.replace('#', '');
           return (
@@ -65,19 +70,21 @@ const Navbar = () => {
               href={section}
               key={sectionId}
               className={`navbar__link ${activeSection === sectionId ? 'active' : ''}`}
+              onClick={() => setIsNavExpanded(false)} // Close the navbar when a link is clicked
             >
-              {sectionId.toUpperCase()}
+              {sectionId.charAt(0).toUpperCase() + sectionId.slice(1)} 
             </a>
           );
         })}
-  <a
-      href={cvFile}
-      className="navbar__link"
-      target="_blank"
-      rel="noopener noreferrer" // Isso é importante por razões de segurança
-    >
-      CV
-    </a>
+        <a
+          href={cvFile}
+          className="navbar__link"
+          target="_blank"
+          rel="noopener noreferrer" // Important for security reasons
+          onClick={() => setIsNavExpanded(false)} // Close the navbar when the CV link is clicked
+        >
+          CV
+        </a>
       </div>
     </nav>
   );
